ee387b5ef58038edc9e5a3129ea41be73f92ce8f
hashcowuwu <3189506476@qq.com>
2024-12-16 20:47:21 +0800

    add ip messsage of debug

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 import figlet from "figlet";
+import express from "express";
 
 const server = Bun.serve({
+  development: true, //开发者模式
   port: 3000,
+  static: {
+    //静态路由
+    "/bun": new Response(Bun.version),
+    "/api/health-check": new Response("All good!"),
+  },
   fetch(req, server) {
     const url = new URL(req.url);
     if (url.pathname === "/") {
       const body = figlet.textSync("Bun!");
+      console.log("server:");
+      console.log(server);
       return new Response(body);
-    }
-    if (url.pathname === "/bun") {
-      return new Response(Bun.version);
     }
     if (url.pathname === "/ip") {
       const ip = server.requestIP(req);
@@ -19,6 +25,8 @@ const server = Bun.serve({
     return new Response("404!");
   },
 });
+
+// Bun v1.1.27+ required
 
 console.log(`Listening on http://localhost:${server.port} ...`);
 console.log(Bun.version);
