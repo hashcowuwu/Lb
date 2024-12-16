f8d07516a2129bb161e590fda9456ec61f3628a3
honghuajun <erfu81856@gmail.com>
2024-12-16 22:43:05 +0800

    create Network winget

--- database.ts
+++ database.ts
@@ -1,6 +1,13 @@
 import { Database } from "bun:sqlite";
 
-{
-  const db = new Database("mydb.sqlite", { create: true });
-  using query = db;
-}
+const db = new Database("mydb.sqlite", { create: true });
+const createTable = db.prepare(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    )
+  `);
+createTable.run();
+
+export default db;
